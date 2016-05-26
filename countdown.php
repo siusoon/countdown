02a7ad3392ea6bb8ebac80f48e5664b4c9c72acd
siusoon <siusoon@gmail.com>
2016-05-26 16:34:15 +0800

    Update countdown.php

--- countdown.php
+++ countdown.php
@@ -17,16 +17,16 @@ font-size: xx-large;
 <body>
 
 <?php
-$here = $_SERVER['PHP_SELF'];
+$now = $_SERVER['PHP_SELF'];
 $refresh = "0.5";
-header("Refresh: $refresh; url=$here");
+header("Refresh: $refresh; url=$now");
 $end_time = mktime(0,0,0,7,1,2037); 
 date_default_timezone_set("Asia/Hong_Kong");
 #echo date("Y.m.d H:i:s T",time())."<br />\n";
 #echo date("Y.m.d H:i:s T",$end_time)."<br />\n";
-$ten_years = 10*(365*24*60*60) + (2*24*60*60) ;
-$do_something = ($end_time + $ten_years) - time();
+$remaining_ten_years = 10*(365*24*60*60) + (2*24*60*60) ;
+$one_country_two_systems = ($end_time + $remaining_ten_years) - time();
 ?>
-<p> <?php print "$do_something";?></p>
+<p> <?php print "$one_country_two_systems";?></p>
 </body>
 </html>
